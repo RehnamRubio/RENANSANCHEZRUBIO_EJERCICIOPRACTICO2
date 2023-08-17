edd10865ffb2e04024164c6a0c0f8cc8d752e81a
RehnamRubio <133930517+RehnamRubio@users.noreply.github.com>
2023-08-16 23:56:17 -0600

    4

--- src/main/java/com/Examen2/examen/db/IClienteRepository.java
+++ src/main/java/com/Examen2/examen/db/IClienteRepository.java
@@ -3,7 +3,7 @@ package com.Examen2.Examen.db;
 
 import com.Examen2.Examen.entities.Cliente;
 import org.springframework.data.repository.CrudRepository;
-
+// para conectar con el crud
 public interface IClienteRepository extends CrudRepository<Cliente, Integer>{
     
 }
